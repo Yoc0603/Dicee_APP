d7cd5d8a2436688fd74d075ae33356d865c9c76a
Yavuz Orhan Candemir <yavuzorhancandemir@gmail.com>
2025-05-19 14:15:20 +0200

    kodda degisiklik

--- Dicee_APP/ViewController.swift
+++ Dicee_APP/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+    @IBOutlet weak var zar1: UIImageView!
+    
+    @IBOutlet weak var zar2: UIImageView!
+    
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")]
+        
+        zar1.image = diceArray[Int.random(in: 0...5)]
+        zar2.image = diceArray[Int.random(in: 0...5)]
+        
     }
-
-
 }
 
